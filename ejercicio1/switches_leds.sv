eb3afd4766bfbdad186327cdeec77ce1de5c0e3b
ValdiviaM <hvaldivia@estudiantec.cr>
2024-08-21 08:15:01 -0600

    agrega ejercicio1

--- ejercicio1/switches_leds.sv
+++ ejercicio1/switches_leds.sv
@@ -5,5 +5,12 @@ module switches_leds(
     input  logic [3:0]  boton_pi,
     output logic [15:0] led_po
     );
+                
+    always @(*) begin
+        led_po[3:0] = switch_pi[3:0] & {4{~boton_pi[0]}};
+        led_po[7:4] = switch_pi[7:4] & {4{~boton_pi[1]}};
+        led_po[11:8] = switch_pi[11:8] & {4{~boton_pi[2]}};
+        led_po[15:12] = switch_pi[15:12] & {4{~boton_pi[3]}};
+    end
 
 endmodule
